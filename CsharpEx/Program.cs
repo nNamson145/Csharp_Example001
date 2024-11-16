7be64b6becea3e947f6c31db2b80afab37836636
NamSon145 <loginson140521@gmail.com>
2024-11-16 21:31:20 +0700

    Add New Project Visual

---
+++ CsharpEx/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Nam Son");
+        }
+    }
+}
